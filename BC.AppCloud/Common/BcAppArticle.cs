91993de8d8cc3ba34561d1fa018e97b73e99a974
bluecolordemon <214909012@qq.com>
2016-06-03 17:22:16 +0800

    正则添加

---
+++ BC.AppCloud/Common/BcAppArticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace BC.AppCloud.Common
+{
+    public static class BcAppArticle
+    {
+        public static string GetStrByFilter(string sourceContent, string pattern, string replacement)
+        {
+            Regex reg = new Regex("NAME=(.+);");
+            string modified = reg.Replace("", "NAME=WANG;");
+            return "";
+        }
+    }
+}
