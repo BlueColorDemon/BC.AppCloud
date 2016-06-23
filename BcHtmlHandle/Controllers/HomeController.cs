f63cb71026e9fe6e03006f6ebe0cf978e57c8593
bluecolordemon <214909012@qq.com>
2016-06-23 18:05:27 +0800

    jsonp 跨域请求

--- BcHtmlHandle/Controllers/HomeController.cs
+++ BcHtmlHandle/Controllers/HomeController.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Ivony.Html;
+using Ivony.Html.Parser;
+
 namespace BcHtmlHandle.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            var html = "";
+            var htmlParser = new JumonyParser();
+            var doc = htmlParser.Parse(html);
+
+            var css_class = doc.Find(".css_class");
+
+
+
+            //ViewBag.Title = "Home Page";
 
             return View();
         }
---
+++ JsonpTest/TestJsonp.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JsonpTest
+{
+    /// <summary>
+    /// TestJsonp 的摘要说明
+    /// </summary>
+    public class TestJsonp : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/javascript";
+            context.Response.Write("callback('Get成功');");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
